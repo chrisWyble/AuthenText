54f7d0b982747b9a0ab34783f9cf0cab7b8314f7
Chris Wyble <chris_wyble@berkeley.edu>
2024-08-07 23:59:59 -0400

    Added the about the team section

--- streamlit_app.py
+++ streamlit_app.py
@@ -117,28 +117,67 @@ if st.session_state.section == "Home":
             st.markdown(f"- {file}")
 
 elif st.session_state.section == "About the Team":
-    st.header("About the Team")
-    col1, col2, col3=st.columns([1,1,1])
+    # st.header("About the Team")
+    st.text("")
+    st.text("")
+    st.markdown(f"""
+            <h1 style='font-size: 32px; text-align: left;'>
+            About the Team</h1>""", unsafe_allow_html=True)
+        
+    st.text("")        
+    
+    col1, col2=st.columns([11,20],vertical_alignment="center", gap='medium')
     with col1:
         st.image("team_pictures/Brendan Ho.png", width=195)
-        st.markdown("""
-            <h2 style='font-size: 24px; text-align: center;'>Brendan Ho</h2>""", unsafe_allow_html=True)
+        st.markdown(f"""
+            <h2 style='font-size: 24px; text-align: left;'>{'&nbsp;'*8}Brendan Ho</h2>""", unsafe_allow_html=True)
+    st.text("")
+    st.text("")
+    st.text("")
     with col2:
+        st.markdown("""I thoroughly enjoyed the program because it allowed me to meet and work with many brilliant individuals, further fostering my passion for data science.
+                    During the project, I played a key role in overseeing the development of machine learning models as well as assisting dataset generation and creating visualizations.
+                    My goal is to leverage my skills in data science to make a meaningful impact in the food or music industry.
+                    In my free time, I enjoy cooking, music, anime/manga, and video games.""", unsafe_allow_html=True)
+
+
+    col1, col2=st.columns([11,20],vertical_alignment="center", gap='medium')
+    
+    with col1:
         st.image("team_pictures/Chris Wyble.png", width=195)
-        st.markdown("""
-            <h2 style='font-size: 24px; text-align: center;'>Chris Wyble</h2>""", unsafe_allow_html=True)
-    with col3:
-        st.image("team_pictures/Terence Pak.png", width=195)
-        st.markdown("""
-            <h2 style='font-size: 24px; text-align: center;'>Terence Pak</h2>""", unsafe_allow_html=True)
+        st.markdown(f"""
+                <h2 style='font-size: 24px; text-align: left;'>{'&nbsp;'*8}Chris Wyble""", unsafe_allow_html=True)
+    st.text("")
+    st.text("")
+    st.text("")
+    with col2:
+        st.markdown("""MIDS had amazing coverage of topics for in-demand skills along side the oppertunity to work with talented peers.
+                    For our capstone project, I lead the data and cloud engineer tasks such as AWS architecture design, developer operations, streamlit development, and assisted in model evaluation.
+                    Going forward, I desire to apply data science to a variety of practical industries, specifically medical, to help others. 
+                    For fun I enjoy participating in pickleball, playing super smash brothers, and watching anime.""", unsafe_allow_html=True)
+
+    col1, col2=st.columns([11,20],vertical_alignment="center", gap='medium')
+
+    with col1:
+        st.image("team_pictures/Terence Pak.png", width=195)   
+        st.markdown(f"""
+            <h2 style='font-size: 24px; text-align: left;'>{'&nbsp;'*8}Terence Pak</h2>""", unsafe_allow_html=True)
+
+    with col2:
+        st.markdown("""This program was an amazing experience with great people, allowing me to build valuable connections and build up a stronger data science foundation.
+                    As the project manager, I managed the project and defined our scope. 
+                    Some of additional tasks I took on included evaluating the model and the datasets, building our presentations, and create visuals for our presentation.
+                    Moving forward, I intend to build a data science career and pursue my passion in the videogame industry.
+                    In my free time, I enjoy baking, watching anime and photography.""", unsafe_allow_html=True)
 
 elif st.session_state.section == "Instructions":
     st.header("Instructions")
     st.markdown("**Please follow the instructions listed below.**")
     st.markdown("- Refer to the 'Choose a file' prompt and click 'Browse files.'")
-    st.markdown("- Locate and select the PDF file that you would like to evaluate and click 'Open'")
+    st.markdown("- Locate and select the PDF file(s) that you would like to evaluate and click 'Open'")
     st.markdown("- Fill out student information including Student ID, First Name, and Last Name")
     st.markdown("- Select the 'Scan Essay' button. The MGT determination will appear below")
+    st.markdown("- If you desire, you may select the 'Archive' button to store and later view the checked essay(s)")
 
 elif st.session_state.section == "Disclaimer":
     st.header("Disclaimer")
